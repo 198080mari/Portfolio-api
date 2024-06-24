3a64410534c74de555fda8c9c435218717f1f8bf
Mari <missionariamariros@gmail.com>
2024-06-24 08:44:49 -0300

    alteração das importações dos assets

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {  //fetch na url
-    const url = 'https://raw.githubusercontent.com/198080mari/Portfolio-js/main/assets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/198080mari/Portfolio-js/mainassets/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()  //retorna convertido em json
 }
